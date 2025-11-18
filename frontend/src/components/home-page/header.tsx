ee8786e3bd55d28dec53fd0c5511e9bfcaced3a9
Marine LANCAR <malancar@f2r6s13.clusters.42paris.fr>
2025-11-18 20:52:57 +0100

    working on homepage

--- frontend/src/components/home-page/header.tsx
+++ frontend/src/components/home-page/header.tsx
@@ -4,17 +4,25 @@ export function Header() {
     return (
         <header className="bg-background sticky top-0 w-full">
             <div className="container-wrapper">
-                <nav className="flex items-center bg-black h-20">
+                <nav className="flex items-center bg-black h-20 text-gray-300">
                     <div className="h-full flex items-center">
                         <button className="" type="button" onClick={() => (window.location.href = "/")}>
-						    <IconChalkboardTeacher className="w-24 h-full text-white" />
+                            <IconChalkboardTeacher className="w-24 h-full" />
                         </button>
-					</div>
-                    <div className="flex items-center justify-center gap-4 h-full ">
-                        <a className="h-full inline-flex items-center justify-center whitespace-nowrap text-xl font-medium transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:bg-primary/10 hover:text-accent-foreground dark:hover:bg-accent/50 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/tournaments">Tournaments</a>
-                        <a className="h-full hover:bg-primary/10 inline-flex items-center justify-center whitespace-nowrap text-xl font-medium transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/votes">Votes</a>
-                        <a className="h-full hover:bg-primary/10 inline-flex items-center justify-center whitespace-nowrap text-xl font-medium transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/admin">Admin</a>
                     </div>
+                    <div className="flex items-center justify-center gap-4 h-full text-lg">
+                        <a className="h-full inline-flex items-center justify-center whitespace-nowrap font-medium transition-all outline-none hover:bg-gradient-to-br hover:from-blue-400 hover:to-purple-500 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/tournaments">Tournaments</a>
+                        <a className="h-full inline-flex items-center justify-center whitespace-nowrap font-medium transition-all outline-none hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/votes">Votes</a>
+                        <a className="h-full inline-flex items-center justify-center whitespace-nowrap font-medium transition-all outline-none hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500 h-8  gap-1.5 px-3 text-white" data-slot="button" href="/admin">Admin</a>
+                    </div>
+                    <div className="w-full p-6 flex gap-6 justify-end">
+                        <div className="p-2.5 border border-2 border-solid transition-all outline-none hover:bg-white hover:text-black">
+                            <button>Sign In</button>
+                        </div>
+                        <div className="transition-all outline-none bg-purple-800 text-white hover:bg-purple-900">
+                            <button className="p-3 " onClick={() => (window.location.href = "/tournaments/1/register")}>Register</button>
+                        </div>
+					</div>
                 </nav>
             </div>
         </header>
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Header } from '@/components/home-page/header'
 import { TournamentCard } from '@/components/tournament/tournament-card'
-
+import { Footer } from '@/components/home-page/footer'
 
 export const Route = createFileRoute('/')({
 	component: WelcomePage,
@@ -14,9 +14,9 @@ function WelcomePage() {
 				<div className="flex flex-col items-center justify-centertext-center">
 					<Header/>
 					{/* Welcome message  */}
-					<div className="pl-3 bg-gradient-to-br from-black via-indigo-900 to-pink-800 w-full h-130 flex flex-col items-start">
+					<div className="pl-3 bg-gradient-to-br from-black via-purple-900 to-pink-300 w-full h-130 flex flex-col items-start">
 						<div className="p-6 w-full h-70">
-							<h1 style={{ fontFamily: "Orbitron" }} className="font-orbitron py-3 text-7xl text-left font-bold text-white">COMPETE IN THE <br/> ULTIMATE GAMING <br/> ARENA</h1>
+							<h1 style={{ fontFamily: "Orbitron" }} className="font-orbitron py-3 text-7xl text-left font-bold text-white">COMPETE IN THE <br/> <span className="bg-gradient-to-r from-[#78D8F5] via-[#8F71F5] to-pink-300 bg-clip-text text-transparent"> ULTIMATE </span>GAMING <br/> ARENA</h1>
 						</div>
 						<div style={{ fontFamily: "Orbitron" }}  className="p-6 text-gray-400">
 							<p>Join thousands of elite gamers in the most prestigious esports tournaments.
@@ -25,9 +25,9 @@ function WelcomePage() {
 						</div>
 						<div className="w-full p-6 flex items-start gap-10">
 							<div className="">
-								<button className="p-3 bg-gradient-to-r from-[#5AD5FA] to-[#8463F2] text-white">JOIN TOURNAMENT</button>
+								<button className="p-3 bg-gradient-to-r from-[#5AD5FA] to-[#8463F2] text-white transition-all hover:brightness-80">JOIN TOURNAMENT</button>
 							</div>
-							<div className="p-2.5 text-white border border-2 border-solid border-white">
+							<div className="p-2.5 text-white border border-2 border-solid border-white hover:bg-white transition-all hover:text-black">
 								<button>WATCH LIVE</button>
 							</div>
 						</div>
@@ -38,7 +38,20 @@ function WelcomePage() {
 							<h2 className="text-bold text-white p-3 text-3xl">ACTIVE TOURNAMENTS</h2>
 							<p className="text-sm text-gray-400">Battle in the most competitive gmaing environment</p>
 						</div>
+						<div className="py-8 flex justify-center w-full h-full flex p-4 gap-4">
+							<div className="h-50 w-100 bg-gray-900 border border-gray-800">
+								<TournamentCard/>
+							</div>
+							<div className="h-50 w-100 bg-gray-900 border border-gray-800">
+								<TournamentCard/>
+							</div><div className="h-50 w-100 bg-gray-900 border border-gray-800">
+								<TournamentCard/>
+							</div>
+						</div>
 					</div>
+					{/* TOP PLAYERS */}
+					
+					<Footer/>
 				</div>
 			</div>
 		</div>
